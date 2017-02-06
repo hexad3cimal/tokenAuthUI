15ade493d0f1194da2c8bec7823ab0245a10ccd4
hexad3cimal <jovin.war@gmail.com>
2017-02-06 23:16:13 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,HostBinding, Input, Directive } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {FlexLayoutModule} from "@angular/flex-layout";
 import { AppComponent } from './app.component';
+import {MaterialModule} from "@angular/material";
+
+@Directive({
+  selector:'[layout]'
+})
+
+export class LayoutDirective{
+  @Input() layout:string;
+  @HostBinding('style.display') display = 'flex';
+
+  @HostBinding('style.flex-direction')
+  get direction(){
+    return (this.layout === 'column') ? 'column':'row';
+  }
+}
+@Directive({
+  selector:'[flex]'
+})
+export class FlexDirective{
+  @Input() shrink:number = 1;
+  @Input() grow:number = 1;
+  @Input() flex:string;
+
+  @HostBinding('style.flex')
+  get style(){
+    return `${this.grow} ${this.shrink} ${this.flex === '' ? '0':this.flex}%`;
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,FlexDirective ,LayoutDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    FlexLayoutModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
